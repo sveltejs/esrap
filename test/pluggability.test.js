2fd1b6884fbb6cafeaad73b19cfba35c496ee31e
andylovescode <andytk2010@gmail.com>
2025-06-09 15:40:00 -0700

    Plugins: Add incredibly basic test

---
+++ test/pluggability.test.js
@@ -0,0 +1,30 @@
+import { expect, test } from 'vitest';
+import { print } from '../src';
+
+/** @import { Handlers } from '../types' */
+test('custom printers work', () => {
+	/** @type {Handlers} */
+	const funkyStringPrinter = {
+		Literal(node, state) {
+			if (typeof node.value === 'string') {
+				state.commands.push(`:) - `);
+
+				state.commands.push(node.value);
+
+				state.commands.push(` - (:`);
+			}
+		}
+	};
+
+	const { code } = print(
+		{
+			type: 'Literal',
+			value: 'testing 123'
+		},
+		{
+			handlers: { ...funkyStringPrinter }
+		}
+	);
+
+	expect(code).toEqual(':) - testing 123 - (:');
+});
