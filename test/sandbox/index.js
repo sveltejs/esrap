065dd05a8ad7346599f36456af99c254bb42586e
Rich Harris <hello@rich-harris.dev>
2025-11-24 18:00:58 -0500

    chore: fix sandbox (#89)

--- test/sandbox/index.js
+++ test/sandbox/index.js
@@ -1,6 +1,6 @@
 // @ts-check
 import { print } from '../../src/index.js';
-import { load } from '../common.js';
+import { acornParse } from '../common.js';
 import fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -9,6 +9,6 @@ import ts from '../../src/languages/ts/index.js';
 const dir = path.resolve(fileURLToPath(import.meta.url), '..');
 const input_js = fs.readFileSync(`${dir}/_input.ts`);
 const content = input_js.toString();
-const { ast, comments } = load(content);
+const { ast, comments } = acornParse(content);
 const { code } = print(ast, ts({ comments }));
 fs.writeFileSync(`${dir}/_output.ts`, code);
