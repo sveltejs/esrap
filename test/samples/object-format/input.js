f98055b1c322bfbf9c6d666688299230d501ded2
jycouet <jycouet@gmail.com>
2026-01-20 18:25:16 +0100

    test & expect

--- test/samples/object-format/input.js
+++ test/samples/object-format/input.js
@@ -1,7 +1,26 @@
-const obj = {
+const obj1 = {
 	foo: 'bar',
 	level11: { level12: { hello: 'world' } },
 	level21: { level22: { world: 'hello' } },
 	hello: 'world',
 	level31: { level32: {} }
+};
+
+const obj2 = {
+	before: 1,
+	object: {
+		a: 'lorem ipsum dolor sit amet',
+		b: 'lorem ipsum dolor sit amet',
+		c: 'lorem ipsum dolor sit amet',
+		d: 'lorem ipsum dolor sit amet',
+		e: 'lorem ipsum dolor sit amet'
+	},
+	array: [
+		'lorem ipsum dolor sit amet',
+		'lorem ipsum dolor sit amet',
+		'lorem ipsum dolor sit amet',
+		'lorem ipsum dolor sit amet',
+		'lorem ipsum dolor sit amet'
+	],
+	after: 2
 };
