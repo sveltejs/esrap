e1004fb512d1bd8012e6cb4137cc7f0b92e8e523
jycouet <jycouet@gmail.com>
2026-04-08 23:02:44 +0200

    back to cleaner

--- src/languages/ts/public.d.ts
+++ src/languages/ts/public.d.ts
@@ -1,2 +1,6 @@
-export * from './index';
-export type { BaseComment, Comment } from '../types';
+import type { Visitors, BaseNode } from '../../types';
+import type { TSOptions, BaseComment, Comment } from '../types';
+export type { BaseComment, Comment };
+export type { TSOptions };
+export default function ts(options?: TSOptions): Visitors<BaseNode>;
+export declare const EXPRESSIONS_PRECEDENCE: Record<string, number>;
--- src/public.d.ts
+++ src/public.d.ts
@@ -1,21 +1,2 @@
 export type { PrintOptions, Visitors } from './types';
-export { Context } from './index';
-
-import type { BaseNode, PrintOptions, Visitors } from './types';
-
-export function print<T extends BaseNode = BaseNode>(
-	node: T,
-	visitors: Visitors<T>,
-	opts?: PrintOptions
-): {
-	code: string;
-	map: any;
-};
-export function print(
-	node: BaseNode,
-	visitors: Record<string, Function>,
-	opts?: PrintOptions
-): {
-	code: string;
-	map: any;
-};
+export * from './index';
