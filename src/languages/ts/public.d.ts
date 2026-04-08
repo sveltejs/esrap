6aa75fe9225515719e5f7fbf39ee2d8549bfa947
jycouet <jycouet@gmail.com>
2026-04-08 23:07:47 +0200

    expose Node back

--- src/languages/ts/public.d.ts
+++ src/languages/ts/public.d.ts
@@ -2,5 +2,6 @@ import type { Visitors, BaseNode } from '../../types';
 import type { TSOptions, BaseComment, Comment } from '../types';
 export type { BaseComment, Comment };
 export type { TSOptions };
+export type Node = BaseNode;
 export default function ts(options?: TSOptions): Visitors<BaseNode>;
 export declare const EXPRESSIONS_PRECEDENCE: Record<string, number>;
--- src/languages/tsx/public.d.ts
+++ src/languages/tsx/public.d.ts
@@ -2,4 +2,5 @@ import type { Visitors, BaseNode } from '../../types';
 import type { TSOptions, BaseComment, Comment } from '../types';
 export type { BaseComment, Comment };
 export type { TSOptions };
+export type Node = BaseNode;
 export default function tsx(options?: TSOptions): Visitors<BaseNode>;
