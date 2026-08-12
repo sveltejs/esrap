3cbb677462113eec25f60bbcdbaa4507b741f7e2
Simon H <5968653+dummdidumm@users.noreply.github.com>
2026-08-12 22:59:35 +0200

    fix: preserve parantheses of jsdoc type casts (#162)
    Without this jsdoc type casts are lost in the output.

---
+++ .changeset/cool-snails-laugh.md
@@ -0,0 +1,5 @@
+---
+'esrap': patch
+---
+
+fix: preserve parantheses of jsdoc type casts
---
+++ test/samples/comment-jsdoc-type-cast/expected.js
@@ -0,0 +1,2 @@
+const foo = /** @type {number} */ (1);
+const bar = /** @type {number} */ (/** @type {number} */ (1));
