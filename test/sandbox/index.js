6f3624f9c5dd1336f7f06407a61e3e78c9c5fe06
Rich Harris <richard.a.harris@gmail.com>
2026-09-24 14:53:13 -0400

    sandbox: write AST before printing

--- test/sandbox/index.js
+++ test/sandbox/index.js
@@ -10,10 +10,10 @@ const dir = path.resolve(fileURLToPath(import.meta.url), '..');
 const input_js = fs.readFileSync(`${dir}/_input.ts`);
 const content = input_js.toString();
 const { ast, comments } = acornParse(content);
+fs.writeFileSync(`${dir}/_input.json`, JSON.stringify(ast, null, '\t'));
 const { code, map } = print(ast, ts({ comments }), {
 	sourceMapSource: '_output.js',
 	sourceMapContent: content
 });
-fs.writeFileSync(`${dir}/_input.json`, JSON.stringify(ast, null, '\t'));
 fs.writeFileSync(`${dir}/_output.js`, code);
 fs.writeFileSync(`${dir}/_output.js.map`, map.toString());
